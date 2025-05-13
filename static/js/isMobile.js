04b2f9323d7f01fb70d9c7cb3429a562383d6a82
Javier-Programming <javierdiazmsound@gmail.com>
2025-05-13 12:20:07 -0500

    Actualización

--- static/js/isMobile.js
+++ static/js/isMobile.js
@@ -1,42 +1,20 @@
-// (function () {
-//   const getCurrentPage = () => window.location.pathname.split("/").pop();
-//   const isMobileScreen = () => window.innerWidth <= 600;
-
-//   const redirectIfNeeded = () => {
-//     const currentPage = getCurrentPage();
-
-//     if (isMobileScreen() && currentPage !== "/mobile.html") {
-//       window.location.replace("/mobile.html");
-//     } else if (!isMobileScreen() && currentPage !== "") {
-//       window.location.replace("/");
-//     }
-//   };
-
-//   redirectIfNeeded();
-
-//   window.addEventListener("resize", () => {
-//     redirectIfNeeded();
-//   });
-// })();
-
 (function () {
-  const isMobileScreen = window.innerWidth <= 600;
-  const currentPath = window.location.pathname;
+  const getCurrentPage = () => window.location.pathname.split("/").pop();
+  const isMobileScreen = () => window.innerWidth <= 600;
 
-  if (isMobileScreen && !currentPath.endsWith("/mobile")) {
-    window.location.replace("/mobile"); // 👈 sin .html
-  } else if (!isMobileScreen && currentPath !== "/") {
-    window.location.replace("/");
-  }
+  const redirectIfNeeded = () => {
+    const currentPage = getCurrentPage();
 
-  window.addEventListener("resize", () => {
-    const isNowMobile = window.innerWidth <= 600;
-    const pageNow = window.location.pathname;
-
-    if (isNowMobile && !pageNow.endsWith("/mobile")) {
-      window.location.replace("/mobile"); // 👈 sin .html
-    } else if (!isNowMobile && pageNow !== "/") {
+    if (isMobileScreen() && currentPage !== "/mobile.html") {
+      window.location.replace("/mobile.html");
+    } else if (!isMobileScreen() && currentPage !== "") {
       window.location.replace("/");
     }
+  };
+
+  redirectIfNeeded();
+
+  window.addEventListener("resize", () => {
+    redirectIfNeeded();
   });
 })();
